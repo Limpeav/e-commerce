1d76bb0a2bff189df964a7dc86c4a9b1a8b71867
Limpeav <157132138+Limpeav@users.noreply.github.com>
2026-01-20 14:27:47 +0700

    Delete ecommerce-backend directory

--- ecommerce-backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getDashboardData } from "../controllers/adminController.js";
-import { protect, admin } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/dashboard", protect, admin, getDashboardData);
-
-export default router;
