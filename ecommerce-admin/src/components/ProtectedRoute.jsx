4cdaacdba2741a68a0940bf1f4cb01d49365da85
Limpeav <157132138+Limpeav@users.noreply.github.com>
2026-01-20 14:27:32 +0700

    Delete ecommerce-admin directory

--- ecommerce-admin/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const admin = JSON.parse(localStorage.getItem("admin"));
-  return admin?.token ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
