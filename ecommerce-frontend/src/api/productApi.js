26b9e613662af81e4e9b5b4079e8807fb2eeb02b
Limpeav <157132138+Limpeav@users.noreply.github.com>
2026-01-20 14:27:57 +0700

    Delete ecommerce-frontend directory

--- ecommerce-frontend/src/api/productApi.js
+++
@@ -1,6 +0,0 @@
-export const fetchProducts = async () => {
-  //const res = await fetch("https://fakestoreapi.com/products");
-  const res = await fetch("http://localhost:4000/api/products");
-  if (!res.ok) throw new Error("Failed to fetch products");
-  return res.json();
-};
